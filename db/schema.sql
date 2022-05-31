4f466480ac0f1cb9780829c7977912747cf29d43
ester kim <kimester1996@gmail.com>
2022-05-31 09:48:58 -0800

    fixing files and get/post routes working

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS coolgame;
+CREATE DATABASE coolgame;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path')
+const sequelize = require('./config/connection')
+const routes = require('./routes')
+const helpers= require('.utils/helpers')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../client.build')))
+}
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
