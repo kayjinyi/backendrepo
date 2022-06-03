f2b75b273fb708e76660b20ff2395257ee0e4c4e
ester kim <kimester1996@gmail.com>
2022-06-03 11:50:20 -0800

    added cors and asc score

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const path = require('path')
 const sequelize = require('./config/connection')
 const routes = require('./routes')
 const helpers= require('./util/helpers')
-
+const cors= require('cors')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(cors());
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '../client.build')))
-}
+// if(process.env.NODE_ENV === 'production'){
+//   app.use(express.static(path.join(__dirname, '../client.build')))
+// }
 
 app.use(routes);
 
