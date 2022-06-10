3ccb412226f4b61829354fbd825c8d7e264a4a54
ester kim <kimester1996@gmail.com>
2022-06-10 12:21:21 -0800

    backend finalized

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(cors())
-// app.use(cors({
-//   origin:"https://kooler-games.herokuapp.com/"
-// }));
-// if(process.env.NODE_ENV === 'production'){
-//   app.use(express.static(path.join(__dirname, '../client.build')))
-// }
+// app.use(cors())
+app.use(cors({
+  origin:"https://kooler-games.herokuapp.com/"
+}));
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../client.build')))
+}
 
 app.use(routes);
 
